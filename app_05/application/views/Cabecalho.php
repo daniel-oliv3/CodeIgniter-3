b30a7bc044e76113cea0c5b6db9f456f76275c0b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-13 09:52:13 -0300

    Update FunFun projects v3

---
+++ app_05/application/views/Cabecalho.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>App 5</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="img/favicon.ico"/>
+</head>
+<body>
+
+<h3>Este é o Cabeçalho.</h3>
+<hr>
+
+	
